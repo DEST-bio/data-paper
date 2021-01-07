507c625444698d654a8a56c1d3e8ccf26a1d35fa
Alan Bergland <aob2x@virginia.edu>
2021-01-07 08:52:52 -0500

    updates

--- Figure3/Figure_1.R
+++ Figure3/Figure_1.R
@@ -23,20 +23,30 @@ setwd("/Users/alanbergland/Documents/GitHub/")
 
 
 ### timeline
-  ### made here: DEST/figures/figure_1/worldMap.Rdata
-  #		save(samps, samps.ag, multi_sample, file="DEST/figures/figure_1/timeline.Rdata")
-  load(file="DEST/figures/figure_1/timeline.Rdata")
+  ### find sites with multiple time points
+    samps.ag <- samps[,list(nSamps=length(locality),
+              nSpring=sum(season=="spring"),
+              nFall=sum(season=="fall"),
+              nTime=length(unique(collectionDate)),
+              maxDelta=max(yday) - min(yday),
+              lat=mean(lat),
+              long=mean(long)),
+          list(locality, year, continent, set)]
 
-      samps[,season:=factor(season, levels=c("spring", "fall", "frost"))]
+    setkey(samps.ag, locality, year)
+    setkey(samps, locality, year)
 
-  		multi_sample <- ggplot() +
-  		geom_line(data= samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality, linetype=continent)) +
-  		geom_point(data=samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality, color=season)) +
-  		facet_grid(.~year) +
-      theme_cowplot() +
-  		theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position="right", legend.direction="vertical") +
-  		scale_x_date(date_labels = "%b", limits = as.Date(c(110,355), origin = as.Date("2018-01-01"))) +
-  		xlab("Collection Date") + ylab("Latitude")
+    samps[,season:=factor(season, levels=c("spring", "fall", "frost"))]
+
+  ### generate plot
+		multi_sample <- ggplot() +
+		geom_line(data= samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality, linetype=continent)) +
+		geom_point(data=samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality, color=season)) +
+		facet_grid(.~year) +
+    theme_cowplot() +
+		theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position="right", legend.direction="vertical") +
+		scale_x_date(date_labels = "%b", limits = as.Date(c(110,355), origin = as.Date("2018-01-01"))) +
+		xlab("Collection Date") + ylab("Latitude")
 
 ### summary stats
   ### DEST summary stat stuff from: DEST/utils/averageReadDepth_GDS.R

f8795b44190389e2c871edf6b475b746da91d723
MartinKapun <capoony@gmail.com>
2021-01-26 23:03:29 +0100

    update Figure Numbering

---
+++ Figure1/Figure1.R
@@ -0,0 +1,165 @@
+### Make basic plots for DEST samples
+### Final data has columns: sampleId, country, city, collectionDate, lat, long, season, nFlies, locality, type (inbred/pooled), continent
+### Alan Bergland, Oct 3, 2018; updated Feb 2020
+
+
+### ijob -c1 -p standard -A berglandlab
+### module load gcc/7.1.0  openmpi/3.1.4 R/3.6.0; R
+
+
+### libraries
+	library(data.table)
+	library(gdata)
+	library(cowplot)
+	library(data.table)
+	library(foreach)
+	library(ggplot2)
+	library(ggmap)
+	library(maps)
+	library(mapdata)
+	library(ggthemes)
+	library(patchwork)
+
+### set working directory
+	#setwd("/scratch/aob2x/dest")
+	setwd("/Users/alanbergland/Documents/GitHub/")
+
+### load meta data
+  samps <- fread("./DEST_freeze1/populationInfo/samps.csv")
+
+### load sequencing summary stats
+	pcr <- fread("./DEST_freeze1/populationInfo/sequencingStats/pcr.csv")
+	rd <- fread("./DEST_freeze1/populationInfo/sequencingStats/rd.csv")[auto==T]
+	simulans <- fread("./DEST_freeze1/populationInfo/sequencingStats/simulans.csv")[auto==T]
+
+	setkey(rd, sampleId)
+	setkey(pcr, sampleId)
+	setkey(simulans, sampleId)
+	mps <- merge(rd, pcr)
+	mps <- merge(mps, simulans)
+	### quick fix for Ukrainian samples
+		mps[sampleId=="UA_Pir_14_26", sampleId:="UA_Pyr_14_26"]
+		mps[sampleId=="UA_Pir_15_21", sampleId:="UA_Pyr_15_21"]
+		mps[sampleId=="UA_Pyr_16_48", sampleId:="UA_Pir_16_48"]
+
+	mps <- merge(mps, samps, all.y=T, by="sampleId")
+	setnames(mps, "mu.25", "AveReadDepth.25")
+
+	### a few small fixes
+	  mps[continent=="North_America", continent:="NorthAmerica"]
+
+	  mps[,effRD.25:=(AveReadDepth.25 * 2*nFlies) / (AveReadDepth.25 + 2*nFlies)]
+
+	### rank x-axis to mean read depth
+	  mps <- mps[auto.x==T]
+	  mps[,x:=rank(AveReadDepth.25, ties.method="first")]
+	  mps[,x.id:=factor(sampleId, levels=mps$sampleId[mps$x])]
+
+	  mps[,propMissing:=nmissing.25/10000]
+
+		setnames(mps, "AveReadDepth.25", "AveReadDepth")
+		setnames(mps, "auto.x", "auto")
+		setnames(mps, "effRD.25", "effRD")
+	
+	### wide to long
+	  mpsl <- melt(mps,
+	              id.vars=c("x", "sampleId", "continent", "auto", "set"),
+	              measure.vars=c("AveReadDepth", "propMissing", "nFlies", "effRD", "pcrDup", "propSimNorm"))
+
+	  mpsl[,xf:=as.factor(x)]
+
+
+
+
+
+
+### time plot
+	### find sites with multiple time points
+		samps.ag <- samps[,list(nSamps=length(locality),
+							nSpring=sum(season=="spring"),
+							nFall=sum(season=="fall"),
+							nTime=length(unique(collectionDate)),
+							maxDelta=max(yday) - min(yday),
+							lat=mean(lat),
+							long=mean(long)),
+					list(locality, year, continent, set)]
+
+		setkey(samps.ag, locality, year)
+		setkey(samps, locality, year)
+
+
+### world map plot
+
+	world <- as.data.table(map_data("world"))
+
+	samps.ag.ag <- samps.ag[,list(n=sum(nTime), lat=mean(lat), long=mean(long)), list(locality, set)]
+	samps.ag.ag[,set:=factor(set, levels=c("DrosEU", "DrosRTEC", "dgn"))]
+
+### make maps
+
+	min.lat.eu <- 35
+	max.lat.eu <- 55
+	min.long.eu <- -10
+	max.long.eu <- 37
+	# [long>=min.long.eu & long<= max.long.eu][lat>=min.lat.eu & lat<=max.lat.eu]
+	#[longitude>=min.long.eu & longitude<= max.long.eu][latitude>=min.lat.eu & latitude<=max.lat.eu]
+
+
+	### summary plot
+	  summaryStat.plot <- ggplot(data=mpsl, aes(x=xf, y=value, color=continent, fill=continent)) +
+	  geom_point(pch=21, alpha=.95, size=.75) +
+	  facet_grid(variable~set, scales="free", space="free_x") +
+	  theme_few() + scale_color_tableau() +
+	  theme(axis.text.x = element_blank(),
+					panel.spacing = unit(.25, "lines"),
+					axis.ticks.x = element_blank(),
+					legend.position="bottom",
+				  legend.text=element_text(size=8),
+					axis.text.y = element_text(size=8)) +
+	  xlab("Population") + ylab("")
+
+	### plot multi-sample populations
+
+		multi_sample <- ggplot() +
+		geom_line(data= samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality, linetype=continent)) +
+		geom_point(data=samps[J(samps.ag[maxDelta>10])], aes(x=as.Date(yday, origin = as.Date("2018-01-01")), y=lat, group=locality)) +
+		facet_grid(.~year) +
+		scale_x_date(date_labels = "%b", limits = as.Date(c(110,355), origin = as.Date("2018-01-01"))) +
+		xlab("Collection Date") + ylab("Latitude") +
+		theme_bw() + scale_colour_colorblind() +
+		theme(axis.text = element_text(angle = 45, hjust = 1, size=8),
+					legend.text=element_text(size=8))
+
+
+	 world.plot <- 	ggplot() +
+	 geom_polygon(data = world,
+	 			aes(x=long, y = lat, group = group), fill="lightgrey") +
+	 geom_point(data = samps.ag.ag,
+	 			aes(x=long, y=lat, size=I((n-1)/5 + 1), color=set), alpha=.75) +
+	 xlab("Longitude") + ylab("Latitude") + scale_fill_manual(values="black") +
+	 theme_bw() + scale_colour_colorblind() +
+	 theme(axis.text = element_text(angle = 45, hjust = 1, size=8),
+			   legend.text=element_text(size=8))
+
+
+
+	#ggsave(world.plot, file="./DEST/populationInfo/worldPlot.pdf", height=4, width=6)
+
+
+
+### big plot
+layout <- "
+AAAAACC
+AAAAACC
+AAAAACC
+BBBBBCC
+BBBBBCC
+BBBBBCC"
+
+bigplot <- world.plot + multi_sample + summaryStat.plot +
+plot_annotation(tag_levels = 'A', theme=theme(plot.tag = element_text(size = 19))) +
+plot_layout(design = layout)
+
+
+
+ggsave(bigplot, file="~/Figure3.pdf", h=7, w=9)
---
+++ Figure2/MakeFigure.r
@@ -0,0 +1,174 @@
+library(tidyverse)
+library(gridExtra)
+library(scales)
+
+DATA.poolsnp=read.table("~/GitHub/data-paper/Figure4/data/PoolSNP.pnps.gz",
+                        header=T,
+                        na.strings = "NA")
+#summary(DATA.poolsnp)
+LIST=as.character(read.table("~/GitHub/data-paper/Figure4/data/exlcude.txt",
+                             header=F)[,1])
+
+DATA.poolsnp.group <- DATA.poolsnp %>%
+  filter(Chrom =="genomewide" & MAF==0.01) %>%
+  group_by(MAC) %>%
+  summarise(
+    n=n(),
+    pnps.m = mean(pNpS), 
+    pnps.sd=sd(pNpS),
+    pnps.se =sd(pNpS)/sqrt(sum(n()))
+  )
+
+DATA.poolsnp.group$NAME<-rep("PoolSNP",
+                             nrow(DATA.poolsnp.group))
+
+DATA.poolsnp.plot <- ggplot(DATA.poolsnp.group,
+                            aes(x=MAC,y=pnps.m))+
+  geom_jitter(data=filter(DATA.poolsnp,Chrom=="genomewide" & MAF==0.01),
+              aes(x=MAC,y=pNpS),
+              col=rgb(0,0,0,0.1))+
+  geom_ribbon(aes(ymin = pnps.m-pnps.sd, ymax = pnps.m+pnps.sd), 
+              colour = NA,
+              fill=alpha(c("blue"),alpha=0.2))+
+  geom_line(lwd=1.5,
+            col="blue")+
+  xlab("Minor allele count (MAC)")+
+  ylab("pN/pS")+
+  #geom_abline(slope = 0,intercept=0.4454857483957618,lty=2,col="red",lwd=1.5)+
+  theme_bw()+
+  theme(axis.title.y = element_text(size = 26, angle = 90),
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text=element_text(size=18),
+        strip.text =element_text(size=20))+
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.05),
+                     limits=c(0,2.5))+
+  facet_wrap(vars(NAME))
+DATA.poolsnp.plot
+
+DATA.poolsnp.group.f <- DATA.poolsnp %>%
+  filter(!POP %in% LIST & Chrom =="genomewide" & MAF==0.01) %>%
+  group_by(MAC) %>%
+  summarise(
+    n=n(),
+    pnps.m = mean(pNpS), 
+    pnps.sd=sd(pNpS),
+    pnps.se =sd(pNpS)/sqrt(sum(n()))
+  )
+
+DATA.poolsnp.group.f$NAME<-rep("PoolSNP",nrow(DATA.poolsnp.group.f))
+
+DATA.poolsnp.plot.f <- ggplot(DATA.poolsnp.group.f,
+                              aes(x=MAC,y=pnps.m))+
+  geom_jitter(data=filter(DATA.poolsnp,Chrom=="genomewide" & MAF==0.01 & !POP %in% LIST),
+              aes(x=MAC,y=pNpS),
+              col=rgb(0,0,0,0.1))+
+  geom_ribbon(aes(ymin = pnps.m-pnps.sd, ymax = pnps.m+pnps.sd), 
+              colour = NA,
+              fill=alpha(c("blue"),alpha=0.2))+
+  geom_line(lwd=1.5,
+            col="blue")+
+  xlab("Minor allele count (MAC)")+
+  ylab("pN/pS")+
+  #geom_abline(slope = 0,intercept=0.4454857483957618,lty=2,col="red",lwd=1.5)+
+  theme_bw()+
+  theme(axis.title.y = element_text(size = 26, angle = 90),
+        axis.title.x = element_text(size = 26, angle = 00),
+        axis.text=element_text(size=18))+ 
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.05),
+                     limits=c(0.2,0.6))
+DATA.poolsnp.plot.f
+
+DATA.snape=read.table("~/GitHub/data-paper/Figure4/data/SNAPE.pnps.gz",
+                      header=T,
+                      stringsAsFactors = F)
+summary(DATA.snape)
+
+DATA.snape.group <- DATA.snape %>%
+  filter(Chrom =="genomewide") %>%
+  group_by(MAF,Chrom) %>%
+  summarise(
+    n=n(),
+    pnps.m = mean(pNpS), 
+    pnps.sd=sd(pNpS),
+    pnps.se =sd(pNpS)/sqrt(sum(n()))
+  )
+DATA.snape.group    
+
+DATA.snape.group$NAME<-rep("SNAPE",nrow(DATA.snape.group))
+
+null.l=read.table("~/GitHub/data-paper/Figure4/data/null.pnps",header=F)
+colnames(null.l)<-c("x","Chrom","NS","SS","y")
+
+NL<-filter(null.l,Chrom == "genomewide")
+
+DATA.snape.plot <- ggplot(DATA.snape.group,
+                          aes(x=MAF,y=pnps.m))+
+  geom_jitter(data=filter(DATA.snape,Chrom=="genomewide"),
+              aes(x=MAF,y=pNpS),
+              col=rgb(0,0,0,0.1))+
+  geom_ribbon(aes(ymin = pnps.m-pnps.sd, ymax = pnps.m+pnps.sd), 
+              colour = NA,
+              fill=alpha(c("blue"),alpha=0.2))+
+  geom_line(lwd=1.5,
+            col="blue")+
+  xlab("Minor allele frequency (MAF)")+
+  ylab("pN/pS")+
+  ylim(0,2.5)+
+  #geom_line(DATA.poolsnp=NL,aes(x=x,y=y),lty=2,col="red",lwd=1.5)+
+  theme_bw()+
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        strip.text =element_text(size=20))+
+  facet_wrap(vars(NAME))
+DATA.snape.plot
+
+DATA.snape.group.f <- DATA.snape %>%
+  filter(!POP %in% LIST) %>%
+  filter(Chrom =="genomewide") %>%
+  group_by(MAF,Chrom) %>%
+  summarise(
+    n=n(),
+    pnps.m = mean(pNpS), 
+    pnps.sd=sd(pNpS),
+    pnps.se =sd(pNpS)/sqrt(sum(n()))
+  )
+
+DATA.snape.group.f$NAME<-rep("SNAPE",
+                             nrow(DATA.snape.group.f))
+
+DATA.snape.plot.f <- ggplot(DATA.snape.group.f,
+                            aes(x=MAF,y=pnps.m))+
+  geom_jitter(data=filter(DATA.snape,Chrom=="genomewide"& !POP %in% LIST),
+              aes(x=MAF,y=pNpS),
+              col=rgb(0,0,0,0.1))+
+  geom_ribbon(aes(ymin = pnps.m-pnps.sd, ymax = pnps.m+pnps.sd), 
+              colour = NA,
+              fill=alpha(c("blue"),alpha=0.2))+
+  geom_line(lwd=1.5,
+            col="blue")+
+  xlab("Minor allele frequency (MAF)")+
+  ylab("pN/pS")+
+  ylim(0.2,0.6)+
+  #geom_line(DATA.poolsnp=NL,aes(x=x,y=y),lty=2,col="red",lwd=1.5)+
+  theme_bw()+
+  theme(axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.x = element_text(size = 26, angle = 00),
+        axis.text=element_text(size=18))
+DATA.snape.plot.f
+
+pdf("~/GitHub/data-paper/Figure4/Figure4.pdf",width=15,height=9)
+
+grid.arrange(DATA.poolsnp.plot,
+             DATA.snape.plot,
+             DATA.poolsnp.plot.f,
+             DATA.snape.plot.f,
+             layout_matrix = rbind(c(rep(1,15),rep(2,13)),c(rep(1,15),rep(2,13)),c(rep(1,15),rep(2,13)),c(rep(3,15),rep(4,13)),c(rep(3,15),rep(4,13)))) ## by eyeballing
+dev.off()

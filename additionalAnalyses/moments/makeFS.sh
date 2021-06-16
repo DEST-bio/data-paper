b9a59cb9a1ed24033043b15cd30346b7650714c6
Alan Bergland <aob2x@virginia.edu>
2021-06-16 19:42:15 -0400

    Update makeFS.sh

--- additionalAnalyses/moments/makeFS.sh
+++ additionalAnalyses/moments/makeFS.sh
@@ -13,8 +13,8 @@
 ### run as: sbatch --array=1-$( cat /scratch/aob2x/pairs.csv | sed '1d' | wc -l ) /scratch/aob2x/data-paper/additionalAnalyses/moments/makeFS.sh
 ### run as: sbatch --array=1-4 /scratch/aob2x/data-paper/additionalAnalyses/moments/makeFS.sh
 
-### sacct -j 13029741
-### cat /scratch/aob2x/dest/slurmOutput/makeFS.12825614
+### sacct -j 22795942
+### cat /scratch/aob2x/dest/slurmOutput/makeFS.22795942_1
 module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 
 ## SLURM_ARRAY_TASK_ID=2

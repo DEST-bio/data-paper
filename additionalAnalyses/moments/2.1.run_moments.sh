1eb02d6169c698ab9f0ed280a0be3c59c6ebe1d9
Alan Bergland <aob2x@virginia.edu>
2021-06-17 14:38:27 -0400

    Update 2.1.run_moments.sh

--- additionalAnalyses/moments/2.1.run_moments.sh
+++ additionalAnalyses/moments/2.1.run_moments.sh
@@ -5,8 +5,8 @@
 #SBATCH -N 1 # on one node
 #SBATCH -t 4:00:00 #<= this may depend on your resources
 #SBATCH --mem=5G #<= this may depend on your resources
-#SBATCH -o ./moments.%A_%a.out # Standard output
-#SBATCH -e ./moments.%A_%a.err # Standard error
+#SBATCH -o /scratch/aob2x/slurmOut/moments.%A_%a.out # Standard output
+#SBATCH -e /scratch/aob2x/slurmOut/moments.%A_%a.err # Standard error
 #SBATCH -p largemem
 #SBATCH -A berglandlab
 

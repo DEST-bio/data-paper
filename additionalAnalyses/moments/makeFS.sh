809212b5e0b795dc84693e29562deed188add80d
Alan Bergland <aob2x@virginia.edu>
2021-06-17 14:05:00 -0400

    Update makeFS.sh

--- additionalAnalyses/moments/makeFS.sh
+++ additionalAnalyses/moments/makeFS.sh
@@ -20,4 +20,4 @@ module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
 ## SLURM_ARRAY_TASK_ID=2
 
 Rscript /scratch/aob2x/data-paper/additionalAnalyses/moments/makeFS.R ${SLURM_ARRAY_TASK_ID}
-Rscript /scratch/aob2x/data-paper/additionalAnalyses/moments/makeFS.R 1
+#Rscript /scratch/aob2x/data-paper/additionalAnalyses/moments/makeFS.R 1

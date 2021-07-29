1763f81a00f4d12991b813bc44ff71b98514f17a
Joaquin Nunez <jcbnunez@d-172-25-243-59.dhcp.virginia.edu>
2021-07-29 14:41:43 -0400

    hail mary 3

--- additionalAnalyses/moments/4.1.moments_bounds.py
+++ additionalAnalyses/moments/4.1.moments_bounds.py
@@ -23,8 +23,8 @@ iterations = sys.argv[3]
 Pair_name = sys.argv[4]
 pop_id1 = sys.argv[6]
 pop_id2 = sys.argv[5]
-projection1 = sys.argv[8]
-projection2 = sys.argv[7]
+projection1 = sys.argv[7]
+projection2 = sys.argv[8]
 
 projection1= int(projection1)
 projection2= int(projection2)
